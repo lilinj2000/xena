753e6675db2880680cd2dd552f70e50d8bb56810
lilinj2000 <lilinj2000@gmail.com>
2019-09-06 10:07:23 +0800

    done.

--- src/Main.cc
+++ src/Main.cc
@@ -24,33 +24,25 @@
 //
 //
 
-
-#include <memory>
 #include <iostream>
-#include "src/server.h"
+#include <memory>
+#include "args.hxx"
 #include "soil/json.h"
 #include "soil/log.h"
-#include "args.hxx"
+#include "src/server.h"
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   args::ArgumentParser parser("The order test program.");
-  args::HelpFlag help(
-      parser,
-      "help",
-      "Display this help menu",
-      {'h', "help"});
-  args::ValueFlag<std::string> config(
-      parser,
-      "config",
-      "config file",
-      {'c', "config"});
+  args::HelpFlag help(parser, "help", "Display this help menu", {'h', "help"});
+  args::ValueFlag<std::string> config(parser, "config", "config file",
+                                      {'c', "config"});
 
   try {
     parser.ParseCLI(argc, argv);
-  } catch (args::Help& h) {
+  } catch (args::Help &h) {
     std::cout << parser;
     return 0;
-  } catch (args::ParseError& e) {
+  } catch (args::ParseError &e) {
     std::cerr << e.what() << std::endl;
     std::cerr << parser;
     return 1;
@@ -63,11 +55,19 @@ int main(int argc, char* argv[]) {
 
   std::unique_ptr<xena::Server> server;
 
-  rapidjson::Document doc;
-  soil::json::load_from_file(&doc, config_file);
-  soil::log::init(doc);
+  try {
+    rapidjson::Document doc;
+    soil::json::load_from_file(&doc, config_file);
+    soil::log::init(doc);
 
-  server.reset(new xena::Server(doc));
+    server.reset(new xena::Server(doc));
+  } catch (std::exception& e) {
+    SOIL_ERROR("exception: {}", e.what());
+    return 1;
+  } catch (...) {
+    SOIL_ERROR("caught unknow exception!!!");
+    return 1;
+  }
 
   return 0;
 }
